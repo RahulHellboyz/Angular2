34e3a8d8f9acbdec5bc2578a0fa27ca6d582d6fc
Rahul Ramachandran <rahul.r@v2solutions.com>
2017-06-07 18:46:14 +0530

    Form validation 2

--- app/users/user-form.component.ts
+++ app/users/user-form.component.ts
@@ -9,15 +9,18 @@ import { User } from '../shared/models/user';
 
         {{form.valid}}
 
-        <div class="form-group">
+        <div class="form-group" [ngClass]="{'has-error':name.invalid && name.touched}">
             <input type="text" class="form-control" name="name" required
-            [(ngModel)]="newUser.name" #name="ngModel">
-        </div>
+            [(ngModel)]="newUser.name" #name="ngModel" placeholder="Name">
+            <span class="help-block" *ngIf="name.invalid && name.touched">Name is required!</span>
+        </div>       
 
-        <div class="form-group">
+        <div class="form-group" [ngClass]="{'has-error':username.invalid && username.touched}">
             <input type="text" class="form-control" name="username" required
-            [(ngModel)]="newUser.username" #name="ngModel">
-        </div>
+            [(ngModel)]="newUser.username" #username="ngModel" placeholder="Username">
+
+            <span class="help-block" *ngIf="username.invalid && username.touched">Username is required!</span>
+        </div>        
 
         <button type="submit" class="btn btn-lg btn-block btn-primary" [disabled]="form.invalid">
             Create User
